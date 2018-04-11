2b9d97113c61b15e6524e43e527108aebd53750b
GuilhermeMmota <32147032+GuilhermeMmota@users.noreply.github.com>
2018-04-10 21:46:28 -0300

    Delete firebaseapp.js

--- www/js/firebaseapp.js
+++
@@ -1,9 +0,0 @@
-var config = {
-  apiKey: "AIzaSyBM9h803KM7AhtxFv5jlscEHjPp-POjWwM",
-  authDomain: "bike-8bbe8.firebaseapp.com",
-  databaseURL: "https://bike-8bbe8.firebaseio.com",
-  projectId: "bike-8bbe8",
-  storageBucket: "bike-8bbe8.appspot.com",
-  messagingSenderId: "420328899231"
-};
-firebase.initializeApp(config);
