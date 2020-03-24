4a6f05282ad94e53efa74aee57b135f558e27f2a
GuilhermeMmota <32147032+GuilhermeMmota@users.noreply.github.com>
2020-03-24 20:40:57 -0300

    Update firebaseapp.js

--- platforms/browser/www/js/firebaseapp.js
+++ platforms/browser/www/js/firebaseapp.js
@@ -1,9 +1,9 @@
 var config = {
-  apiKey: "AIzaSyBM9h803KM7AhtxFv5jlscEHjPp-POjWwM",
+  apiKey: ,
   authDomain: "bike-8bbe8.firebaseapp.com",
   databaseURL: "https://bike-8bbe8.firebaseio.com",
   projectId: "bike-8bbe8",
   storageBucket: "bike-8bbe8.appspot.com",
-  messagingSenderId: "420328899231"
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
